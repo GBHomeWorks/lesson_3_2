b4a47694ddc2d375d068fd0ad855628b0b7c33d1
GBHomeWorks <a.a.mikhaylov2015@gmail.com>
2020-02-17 02:24:50 +0300

    Java.Core1 HW3

--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -15,7 +15,7 @@ public class HomeWork3 {
             System.out.println("Загадано число от 0 до 9");
 
             for (int i = 0; i < 3; i++) {
-                System.out.println("Введите чисило\n");
+                System.out.println("Введите число\n");
                 int userNumber = sc.nextInt();
                 if (userNumber > number) {
                     System.out.println("Ваше число больше загаданного");
@@ -31,7 +31,71 @@ public class HomeWork3 {
             play = sc.nextInt();
         } while (play == 1);
         System.out.println("Игра закончена");
+
+// --------------------------------------------------------------------------------------------------------------
+
+    /*
+
+   // Попытка решения второй задачи ))
+
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+                "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+//        int a = words.length;
+        for (int i = 0; i < words.length;) {
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("%15s",words[i]);
+                i++;
+            }
+            System.out.println();
+        }
+        int wordID = rand.nextInt(words.length);
+//        System.out.println(wordNumber);
+        String word = words[wordID];
+//        System.out.println(word);
+
+        System.out.println("Введите загаданное слово");
+        String userWord = sc.nextLine();
+//        System.out.println(userWord);
+
+        boolean quest = true;
+
+        while (quest) {
+            if (word.equals(userWord)) {
+                quest = false;
+                System.out.println("Вы угадали!");
+            }
+
+            else if (word.length() < userWord.length()) {
+                for (int i = 0; i < 15; i++) {
+                    char a = word.charAt(i);
+                    char b = userWord.charAt(i);
+                    if (a == b) {
+                        System.out.print(a);
+                    } else {
+                        System.out.print("#");
+                    }
+                }
+            } else {
+                for (int i = 0; i < 15; i++) {
+                    char a = word.charAt(i);   // Здесь и выше (в for) вызывается несуществующий элемент [i]. Выдается ошибка.
+                                                // Таким образом видно, что подход не правильный, т.е. нужно изменять логику
+                                                // программы или использовать какой-либо ограничитель счетчика. Задача не решена.
+                    char b = userWord.charAt(i);
+                    if (a == b) {
+                        System.out.print(b);
+                    } else {
+                        System.out.print("#");
+                    }
+                }
+            }
+            quest = false;
+        }
+
+        */
+
     }
 }
+
 
 
